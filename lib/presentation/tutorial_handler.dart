a1d0d6166782a3b570cccb29f116310d1366832a
Sebastian <sebastian.franz@tum.de>
2024-07-23 19:25:22 +0200

    Refactoring overlay widgets and adding AbsorbPointer
    AbsorbPointer makes all other widgets disabled while the overlay is shown

--- lib/presentation/tutorial_handler.dart
+++ lib/presentation/tutorial_handler.dart
@@ -23,13 +23,7 @@ class TutorialHandler {
   void startTutorial() {
     _tutorialBloc.stream.listen((event) {
       if (event.currentTutorialStep != null) {
-        switch (event.currentTutorialStep.runtimeType) {
-          case WidgetHighlightTutorialStep:
-            createHighlightOverlay(
-                tutorialStep: event.currentTutorialStep as WidgetHighlightTutorialStep, borderColor: Colors.red);
-          case PlainTextTutorialStep:
-            createTextOverlay(tutorialStep: event.currentTutorialStep as PlainTextTutorialStep);
-        }
+        createOverlay(event.currentTutorialStep!);
       } else {
         removeOverlayEntry();
       }
@@ -64,166 +58,133 @@ class TutorialHandler {
     return (context, state);
   }
 
-  void createHighlightOverlay({required WidgetHighlightTutorialStep tutorialStep, required Color borderColor}) {
+  void createOverlay(TutorialStep tutorialStep) {
     (BuildContext, NavigatorState)? contextAndState = getCurrentContextAndState();
-    if(contextAndState == null) {
-      return;
-    }
+    if (contextAndState == null) return;
+
     BuildContext context = contextAndState.$1;
     NavigatorState state = contextAndState.$2;
 
+    OverlayContent? content = switch (tutorialStep) {
+      WidgetHighlightTutorialStep whtStep => createHighlightOverlayContent(context, whtStep),
+      PlainTextTutorialStep pttStep => createTextOverlayContent(context, pttStep),
+      _ => null
+    };
+
+    if (content != null) {
+      overlayEntry = OverlayEntry(
+        builder: (BuildContext context) => buildOverlayContent(context, content),
+      );
+
+      state.overlay?.insert(overlayEntry!);
+    }
+  }
+
+  Widget buildOverlayContent(BuildContext context, OverlayContent content) {
+    return Stack(
+      children: [
+        Positioned.fill(
+          child: AbsorbPointer(
+            child: Container(color: Colors.transparent),
+          ),
+        ),
+        ClipPath(
+          clipper: ExclusionClipper(content.exclusionRect),
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
+            child: Container(
+              width: SizeConfig.screenWidth(context),
+              height: SizeConfig.screenHeight(context),
+              color: Colors.black.withOpacity(0.5),
+            ),
+          ),
+        ),
+        _buildTutorialText(content),
+        _buildNextButton(content),
+      ],
+    );
+  }
+
+  Widget _buildTutorialText(OverlayContent content) {
+    return Positioned(
+      left: content.textPosition.dx,
+      top: content.textPosition.dy,
+      child: Container(
+        color: Colors.black,
+        child: DefaultTextStyle(
+          style: tutorialTextStyle,
+          child: Text(content.tutorialText),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildNextButton(OverlayContent content) {
+    return Positioned(
+      left: content.buttonPosition.dx,
+      top: content.buttonPosition.dy,
+      child: SizedBox(
+        width: buttonWidth,
+        height: buttonHeight,
+        child: ElevatedButton(
+          onPressed: nextTutorialElement,
+          style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
+          child: Text("Next", style: tutorialTextStyle),
+        ),
+      ),
+    );
+  }
+
+  OverlayContent createHighlightOverlayContent(BuildContext context, WidgetHighlightTutorialStep tutorialStep) {
     GlobalKey? widgetKey = tutorialStep.loadFromRepository?.call();
     if (widgetKey == null || widgetKey.currentContext == null) {
-      return;
+      throw Exception("Widget key not found for highlight overlay");
     }
-    BuildContext currentContext = widgetKey.currentContext!;
-    RenderBox widgetRenderBox = currentContext.findRenderObject() as RenderBox;
+
+    RenderBox widgetRenderBox = widgetKey.currentContext!.findRenderObject() as RenderBox;
     Offset widgetPosition = widgetRenderBox.localToGlobal(Offset.zero);
     double widgetCenterX = widgetPosition.dx + widgetRenderBox.size.width / 2;
 
-    // Pre-Render Text to get expected size
-    final Size tutorialTextSize = (TextPainter(
-            text: TextSpan(text: tutorialStep.tutorialText, style: tutorialTextStyle),
-            maxLines: 1,
-            //textScaler: MediaQuery.of(context).textScaler, TODO ?
-            textDirection: TextDirection.ltr)
-          ..layout())
-        .size;
+    Size tutorialTextSize = _calculateTextSize(context, tutorialStep.tutorialText);
 
-    // Next-Button
-    double buttonPositionLeft = widgetCenterX - buttonWidth / 2;
-    double buttonPositionTop = widgetPosition.dy + widgetRenderBox.size.height;
-
-    // Clipping around widget
     double clippingLeftPosition = widgetPosition.dx - overlayOffset.dx;
     double clippingTopPosition = widgetPosition.dy - widgetRenderBox.size.height / 2 - overlayOffset.dy;
     double clippingWidth = widgetRenderBox.size.width + overlayOffset.dx * 2;
     double clippingHeight = widgetRenderBox.size.height * 1.5 + overlayOffset.dy * 2;
 
-    // Text
-    double textPositionLeft = widgetCenterX - tutorialTextSize.width / 1.75; // 1.75 looks better than 2
-    double textPositionTop = clippingTopPosition - tutorialTextSize.height;
-
-    overlayEntry = OverlayEntry(
-      // Create a new OverlayEntry.
-      builder: (BuildContext context) {
-        // Align is used to position the highlight overlay
-        // relative to the NavigationBar destination.
-        return Stack(
-          children: [
-            ClipPath(
-                clipper: ExclusionClipper(
-                  Rect.fromLTWH(clippingLeftPosition, clippingTopPosition, clippingWidth, clippingHeight),
-                ),
-                child: BackdropFilter(
-                    filter: ImageFilter.blur(
-                      sigmaX: 5.0,
-                      sigmaY: 5.0,
-                    ),
-                    child: Container(
-                      width: SizeConfig.screenWidth(context),
-                      height: SizeConfig.screenHeight(context),
-                      color: Colors.black.withOpacity(0.5),
-                    ))),
-            Positioned(
-              left: textPositionLeft,
-              top: textPositionTop,
-              child: Container(
-                  color: Colors.black,
-                  child: DefaultTextStyle(style: tutorialTextStyle, child: Text(tutorialStep.tutorialText))),
-            ),
-            Positioned(
-                left: buttonPositionLeft,
-                top: buttonPositionTop,
-                child: SizedBox(
-                    width: buttonWidth,
-                    height: buttonHeight,
-                    child: ElevatedButton(
-                        onPressed: nextTutorialElement,
-                        style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
-                        child: Text(
-                          "Next",
-                          style: tutorialTextStyle,
-                        ))))
-          ],
-        );
-      },
+    return OverlayContent(
+      exclusionRect: Rect.fromLTWH(clippingLeftPosition, clippingTopPosition, clippingWidth, clippingHeight),
+      textPosition:
+          Offset(widgetCenterX - tutorialTextSize.width / 1.75, clippingTopPosition - tutorialTextSize.height),
+      buttonPosition: Offset(widgetCenterX - buttonWidth / 2, widgetPosition.dy + widgetRenderBox.size.height),
+      tutorialText: tutorialStep.tutorialText,
     );
-    state.overlay?.insert(overlayEntry!);
   }
 
-  void createTextOverlay({required PlainTextTutorialStep tutorialStep}) {
-    (BuildContext, NavigatorState)? contextAndState = getCurrentContextAndState();
-    if(contextAndState == null) {
-      return;
-    }
-    BuildContext context = contextAndState.$1;
-    NavigatorState state = contextAndState.$2;
+  OverlayContent createTextOverlayContent(BuildContext context, PlainTextTutorialStep tutorialStep) {
+    Size tutorialTextSize = _calculateTextSize(context, tutorialStep.tutorialText);
 
-    // Pre-Render Text to get expected size
-    final Size tutorialTextSize = (TextPainter(
-            text: TextSpan(text: tutorialStep.tutorialText, style: tutorialTextStyle),
-            maxLines: 1,
-            textScaler: MediaQuery.of(context).textScaler,
-            textDirection: TextDirection.ltr)
-          ..layout())
-        .size;
+    double overlayPositionLeft = SizeConfig.screenWidth(context) / 2 - tutorialTextSize.width / 1.75;
+    double overlayPositionTop = SizeConfig.screenHeight(context) / 2 + tutorialTextSize.height;
 
-    var overlayPositionLeft = SizeConfig.screenWidth(context) / 2 - tutorialTextSize.width / 1.75;
-    var overlayPositionTop = SizeConfig.screenHeight(context) / 2 + tutorialTextSize.height;
-
-    var textPositionLeft = overlayPositionLeft;
-    var textPositionTop = overlayPositionTop + 50;
-    var buttonPositionLeft = textPositionLeft + tutorialTextSize.width / 2 - buttonWidth / 2;
-    var buttonPositionTop = textPositionTop + 50;
-
-    overlayEntry = OverlayEntry(
-      // Create a new OverlayEntry.
-      builder: (BuildContext context) {
-        // Align is used to position the highlight overlay
-        // relative to the NavigationBar destination.
-        return Stack(
-          children: [
-            ClipPath(
-                clipper: ExclusionClipper(
-                  Rect.fromLTWH(
-                      overlayPositionLeft, overlayPositionTop, tutorialTextSize.width, tutorialTextSize.height + 100),
-                ),
-                child: BackdropFilter(
-                    filter: ImageFilter.blur(
-                      sigmaX: 5.0,
-                      sigmaY: 5.0,
-                    ),
-                    child: Container(
-                      width: SizeConfig.screenWidth(context),
-                      height: SizeConfig.screenHeight(context),
-                      color: Colors.black.withOpacity(0.5),
-                    ))),
-            Positioned(
-              left: textPositionLeft,
-              top: textPositionTop,
-              child: Container(
-                  color: Colors.black,
-                  child: DefaultTextStyle(style: tutorialTextStyle, child: Text(tutorialStep.tutorialText))),
-            ),
-            Positioned(
-                left: buttonPositionLeft,
-                top: buttonPositionTop,
-                child: SizedBox(
-                    width: buttonWidth,
-                    height: buttonHeight,
-                    child: ElevatedButton(
-                        onPressed: nextTutorialElement,
-                        style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
-                        child: Text(
-                          "Next",
-                          style: tutorialTextStyle,
-                        ))))
-          ],
-        );
-      },
+    return OverlayContent(
+      exclusionRect:
+          Rect.fromLTWH(overlayPositionLeft, overlayPositionTop, tutorialTextSize.width, tutorialTextSize.height + 100),
+      textPosition: Offset(overlayPositionLeft, overlayPositionTop + 50),
+      buttonPosition:
+          Offset(overlayPositionLeft + tutorialTextSize.width / 2 - buttonWidth / 2, overlayPositionTop + 100),
+      tutorialText: tutorialStep.tutorialText,
     );
-    state.overlay?.insert(overlayEntry!);
+  }
+
+  Size _calculateTextSize(BuildContext context, String text) {
+    return (TextPainter(
+      text: TextSpan(text: text, style: tutorialTextStyle),
+      maxLines: 1,
+      textScaler: MediaQuery.of(context).textScaler,
+      textDirection: TextDirection.ltr,
+    )..layout())
+        .size;
   }
 
   void removeOverlayEntry() {
@@ -231,6 +192,20 @@ class TutorialHandler {
     overlayEntry?.dispose();
     overlayEntry = null;
   }
+}
+
+class OverlayContent {
+  final Rect exclusionRect;
+  final Offset textPosition;
+  final Offset buttonPosition;
+  final String tutorialText;
+
+  OverlayContent({
+    required this.exclusionRect,
+    required this.textPosition,
+    required this.buttonPosition,
+    required this.tutorialText,
+  });
 }
 
 class ExclusionClipper extends CustomClipper<Path> {
