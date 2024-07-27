a2c7ebfb6e8d75311486da3512e0c598e1ca15c2
Sebastian <sebastian.franz@tum.de>
2024-07-27 17:57:09 +0200

    Adding registrationFunction to TutorialKeyRepository

--- lib/domain/tutorial_key_repository.dart
+++ lib/domain/tutorial_key_repository.dart
@@ -4,20 +4,41 @@ import 'package:tutorial_system/tutorial_system.dart';
 class TutorialKeyRepository {
   final GlobalKey<NavigatorState> globalNavigatorKey;
 
+  final Map<Type, TutorialContainer> _tutorialContainers;
+
   final Map<TutorialID, GlobalKey> _keyMap;
   final Map<TutorialID, Future<bool> Function(Duration)> _conditionMap;
   final Map<TutorialID, BuildContext> _contextMap;
 
   TutorialKeyRepository(this.globalNavigatorKey,
-      {Map<TutorialID, GlobalKey>? keyMap,
-      Map<TutorialID, Future<bool> Function(Duration)>? conditionMap,
-      Map<TutorialID, BuildContext>? contextMap})
-      : _keyMap = keyMap ?? {},
+      {List<TutorialContainer>? tutorialContainers,
+        Map<TutorialID, GlobalKey>? keyMap,
+        Map<TutorialID, Future<bool> Function(Duration)>? conditionMap,
+        Map<TutorialID, BuildContext>? contextMap})
+      : _tutorialContainers = _getTypedMap(tutorialContainers),
+        _keyMap = keyMap ?? {},
         _conditionMap = conditionMap ?? {},
         _contextMap = contextMap ?? {};
 
-  void registerKey(TutorialID widgetID, GlobalKey key) {
-    _keyMap[widgetID] = key;
+  static Map<Type, TutorialContainer> _getTypedMap(List<TutorialContainer>? containers) {
+    Map<Type, TutorialContainer> result = {};
+    for (TutorialContainer tutorialContainer in containers ?? []) {
+      result[tutorialContainer.runtimeType] = tutorialContainer;
+    }
+    return result;
+  }
+
+  void callRegistrationFunction({required Type tutorialType, required dynamic caller, State? state}) {
+    TutorialContainer? tutorialContainer = _tutorialContainers[tutorialType];
+    if (tutorialContainer != null) {
+      tutorialContainer.registrationFunction(this, caller, state: state);
+    }
+  }
+
+  void registerKey(TutorialID widgetID, GlobalKey? key) {
+    if (key != null) {
+      _keyMap[widgetID] = key;
+    }
   }
 
   void registerKeys(Map<TutorialID, GlobalKey> keys) {
@@ -36,6 +57,10 @@ class TutorialKeyRepository {
     _conditionMap[conditionID] = condition;
   }
 
+  void registerConditions(Map<TutorialID, Future<bool> Function(Duration timeout)> conditions) {
+    _conditionMap.addAll(conditions);
+  }
+
   void removeCondition(TutorialID conditionID) {
     _conditionMap.remove(conditionID);
   }
@@ -44,8 +69,14 @@ class TutorialKeyRepository {
     return _conditionMap[conditionID];
   }
 
-  void registerContext(TutorialID contextID, BuildContext buildContext) {
-    _contextMap[contextID] = buildContext;
+  void registerContext(TutorialID contextID, BuildContext? buildContext) {
+    if (buildContext != null) {
+      _contextMap[contextID] = buildContext;
+    }
+  }
+
+  void registerContexts(Map<TutorialID, BuildContext> contexts) {
+    _contextMap.addAll(contexts);
   }
 
   void removeContext(TutorialID contextID) {
