fb68b2f403562eb741f82ad19aee8f2b4c9c0b02
Sebastian <sebastian.franz@tum.de>
2024-07-27 17:56:27 +0200

    Adding getName and registrationFunction to TutorialContainer

--- lib/data/tutorial_container.dart
+++ lib/data/tutorial_container.dart
@@ -1,5 +1,11 @@
+import 'package:flutter/material.dart';
 import 'package:tutorial_system/tutorial_system.dart';
 
 abstract class TutorialContainer {
+
+  String getName();
+
+  void registrationFunction(TutorialKeyRepository tutorialKeyRepository, dynamic caller, {State? state});
+
   List<TutorialStep> get tutorialSteps => throw UnimplementedError();
 }
