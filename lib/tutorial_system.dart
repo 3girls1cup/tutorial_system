f4f5fc694185fec2ae8e2b54344ec9f5675a053e
Sebastian <sebastian.franz@tum.de>
2024-08-13 16:21:57 +0200

    Re-organizing library exports

--- lib/tutorial_system.dart
+++ lib/tutorial_system.dart
@@ -1,12 +1,16 @@
 library tutorial_system;
 
-export 'model/tutorial_runner.dart';
-export 'model/tutorial_id.dart';
-export 'model/tutorial_step.dart';
 export 'bloc/tutorial_bloc.dart';
+
 export 'data/tutorial.dart';
+
 export 'domain/tutorial_repository.dart';
-export 'presentation/tutorial_handler.dart';
+
+export 'model/tutorial_id.dart';
+export 'model/tutorial_runner.dart';
+export 'model/tutorial_step.dart';
+
 export 'presentation/tutorial_aware_widget.dart';
+export 'presentation/tutorial_handler.dart';
 export 'presentation/tutorial_registration_mixin.dart';
 export 'presentation/tutorial_start_button.dart';
